aa2d0a312277b0444a0d2b43f8c61867431b03ab
Karina_Frei <karina.gorodnyk@gmail.com>
2022-01-10 13:45:42 -0600

    All set! Good job :)

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,8 @@ public class HelloWorld {
 
     //Doing some work there #2.
 
+    //Doing even more work #3.
+    //Awesome work is done and ready to be committed!
+    //and ready to be pushed to remote repository!
 
 }
